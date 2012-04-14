f958e57bde23c87a418fea215135c297ce257b8c
Theo <theo@iconara.net>
2012-04-14 23:34:07 +0200

    Added benchmarks for expression simplification

--- spec/benchmark.rb
+++ spec/benchmark.rb
@@ -7,42 +7,63 @@ require 'viiite'
 require 'stream_logic'
 
 
+N = 100_000
+
+def random_list_of_numbers!
+  N.times.map { rand(10_000_000).to_s(36) }.sort
+end
+
 file1 = Tempfile.new('file1')
 file2 = Tempfile.new('file2')
 
-arr1 = 100_000.times.map { rand(10_000_000).to_s(36) }.sort
-arr2 = 100_000.times.map { rand(10_000_000).to_s(36) }.sort
-
-arr1.each { |n| file1.puts(n) }
-arr2.each { |n| file2.puts(n) }
+random_list_of_numbers!.each { |n| file1.puts(n) }
+random_list_of_numbers!.each { |n| file2.puts(n) }
 
 Viiite.bench do |r|
-  n = 1
-
-  r.range_over(['Array', 'File'], :type) do |type|
+  r.range_over(['Array', 'File', 'Expressions'], :type) do |type|
     case type
     when 'Array'
-      a = StreamLogic::Stream.new(arr1)
-      b = StreamLogic::Stream.new(arr2)
+      a = StreamLogic::Stream.new(random_list_of_numbers!)
+      b = StreamLogic::Stream.new(random_list_of_numbers!)
     when 'File'
       a = StreamLogic::Stream.new { file1.rewind; file1 }
       b = StreamLogic::Stream.new { file2.rewind; file2 }
+    when 'Expressions'
+      a = StreamLogic::Stream.new(random_list_of_numbers!)
+      b = StreamLogic::Stream.new(random_list_of_numbers!)
+      c = StreamLogic::Stream.new(random_list_of_numbers!)
+      d = StreamLogic::Stream.new(random_list_of_numbers!)
+      e = StreamLogic::Stream.new(random_list_of_numbers!)
+      f = StreamLogic::Stream.new(random_list_of_numbers!)
+      complex_expr = (a & b & c) | (d & e & f)
+      simple_expr = complex_expr.simplify
     end
 
-    r.report('a & b') do
-      n.times { (a & b).to_a }
-    end
+    case type
+    when 'Array', 'File'
+      r.report('a & b') do
+        (a & b).to_a
+      end
 
-    r.report('a | b') do
-      n.times { (b | a).to_a }
-    end
+      r.report('a | b') do
+        (b | a).to_a
+      end
 
-    r.report('a + b') do
-      n.times { (b + a).to_a }
-    end
+      r.report('a + b') do
+        (b + a).to_a
+      end
 
-    r.report('a - b') do
-      n.times { (b - a).to_a }
+      r.report('a - b') do
+        (b - a).to_a
+      end
+    when 'Expressions'
+      r.report('((a & b) & c) | ((d & e) & f)') do
+        complex_expr.to_a
+      end
+
+      r.report('(a & b & c) | (d & e & f)') do
+        simple_expr.to_a
+      end
     end
   end
 end
