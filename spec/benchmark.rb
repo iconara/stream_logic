cb318c421f685b6786c2a8c64611d37e9f0f39a3
Theo <theo@iconara.net>
2012-04-14 20:39:20 +0200

    Refactored the benchmark

--- spec/benchmark.rb
+++ spec/benchmark.rb
@@ -16,22 +16,24 @@ arr2 = 100_000.times.map { rand(10_000_000).to_s(36) }.sort
 arr1.each { |n| file1.puts(n) }
 arr2.each { |n| file2.puts(n) }
 
-stream1 = StreamLogic::Stream.new(arr1)
-stream2 = StreamLogic::Stream.new(arr2)
-stream3 = StreamLogic::Stream.new { file1.rewind; file1 }
-stream4 = StreamLogic::Stream.new { file2.rewind; file2 }
-
 Viiite.bench do |r|
-  r.report('Array stream1 & stream2') do
-    (stream1 & stream2).each { |e| }
-  end
-  r.report('Array stream1 | stream2') do
-    (stream1 | stream2).each { |e| }
-  end
-  r.report('File stream1 & stream2') do
-    (stream3 & stream4).each { |e| }
-  end
-  r.report('File stream1 | stream2') do
-    (stream3 | stream4).each { |e| }
+  n = 1
+
+  r.range_over(['Array', 'File'], :type) do |type|
+    case type
+    when 'Array'
+      a = StreamLogic::Stream.new(arr1)
+      b = StreamLogic::Stream.new(arr2)
+    when 'File'
+      a = StreamLogic::Stream.new { file1.rewind; file1 }
+      b = StreamLogic::Stream.new { file2.rewind; file2 }
+    end
+
+    r.report('a & b') do
+      n.times { (a & b).to_a }
+    end
+    r.report('a | b') do
+      n.times { (b | a).to_a }
+    end
   end
 end
